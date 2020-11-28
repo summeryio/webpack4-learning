d744838175e276f217f547912c130cfc01756eff
summeryio <summeryio@summeryiodeiMac.local>
2020-11-28 14:44:24 +0800

    使用html-webpack-plugin生成index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -39,6 +40,11 @@ module.exports = {
       ]
     }]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
