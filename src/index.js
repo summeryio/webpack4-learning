d0ed59a47f4d13d18fa340a231eae58fff62ff91
summeryio <summeryio@summeryiodeiMac.local>
2020-11-28 13:58:43 +0800

    css modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import avatar from './avatar.jpg'
-import './index.scss'
+import style from './index.scss'
 
 let img = new Image()
 let root = document.getElementById('root')
 
 img.src = avatar
-img.classList.add('avatar')
+img.classList.add(style.avatar)
 
 root.append(img)
