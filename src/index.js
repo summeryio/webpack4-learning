7115210fb9a8a5e30b7dff21368b1e83c1bcc8a1
summeryio <summeryio@summeryiodeiMac.local>
2020-11-29 23:01:07 +0800

    Hot Module Replacement 热模块更新 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import './style.css'
 
-console.log('hello world! ');
+const button = document.createElement('button')
+button.innerHTML = 'add'
+document.body.appendChild(button)
+
+button.addEventListener('click', () => {
+  const div = document.createElement('div')
+  div.innerHTML = 'item'
+  document.body.append(div)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -10,8 +11,10 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist',
-    open: true,
-    port: 8080
+    open: false,
+    port: 8080,
+    hot: true,
+    hotOnly: true
   },
   module: {
     rules: [{
@@ -45,13 +48,23 @@ module.exports = {
         'sass-loader',
         'postcss-loader'
       ]
+    },{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader'
+      ]
     }]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     publicPath: '/',
