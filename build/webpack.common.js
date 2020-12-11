6c9b531bc8d998836081b1b5a0feff082c188e24
summeryio <1005526074@qq.com>
2020-12-11 17:23:51 +0800

    SplitChunksPlugin 配置参数一

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -59,7 +59,21 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: 'all'
+      chunks: "all", // initial async all
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          filename: 'vendors.js'
+        },
+        default: false
+      }
     }
   },
   output: {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-function getComponent() {
-  return import('lodash').then(({default: _}) => {
-    let element = document.createElement('div')
-    element.innerHTML = _.join(['aaa', 'bbb', 'ccc'], '|')
-    return element
-  })
-}
+// 异步 async 引入lodash
+// function getComponent() {
+//   return import(/*webpackChunkName: 'lodash'*/'lodash').then(({default: _}) => {
+//     let element = document.createElement('div')
+//     element.innerHTML = _.join(['aaa', 'bbb', 'ccc'], '|')
+//     return element
+//   })
+// }
 
-getComponent().then(element => {
-  document.body.appendChild(element)
-})
+// getComponent().then(element => {
+//   document.body.appendChild(element)
+// })
+
+
+// 同步 引入lodash
+import _ from 'lodash'
+let element = document.createElement('div')
+element.innerHTML = _.join(['aaa', 'bbb', 'ccc'], '|')
+document.body.appendChild(element)
