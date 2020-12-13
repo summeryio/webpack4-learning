7644496fdc8e3f582f318b09707c456b79ed249a
summeryio <summeryio@summeryiodeiMac.local>
2020-12-13 08:43:29 +0800

    code splitting 简化版配置

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -59,23 +59,7 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: "all",
-      minSize: 30000,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          priority: -20,
-          reuseExistingChunk: true,
-        }
-      }
+      chunks: "all"
     }
   },
   output: {
