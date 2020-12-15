f484390441bc5836adcd64ba5bbe2123fda97edb
summeryio <summeryio@summeryiodeiMac.local>
2020-12-15 21:58:22 +0800

    update

---
+++ src/click.js
@@ -0,0 +1,7 @@
+function handleClick() {
+  let element = document.createElement('div')
+  element.innerHTML = 'summery'
+  document.body.appendChild(element)
+}
+
+export default handleClick
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-async function getComponent() {
-  const {default: _} = await import(/*webpackChunkName: 'lodash'*/'lodash')
-  let element = document.createElement('div')
-  element.innerHTML = _.join(['aaa', 'bbb', 'cccdd'], '|')
-  return element
-}
-
 document.addEventListener('click', () => {
-  getComponent().then(element => {
-    document.body.appendChild(element)
+  import('./click.js').then(({default: fn}) => {
+    fn()
   })
 })
