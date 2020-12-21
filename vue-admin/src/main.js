d7879bc8f8b83c45987e69353524e550732a7454
summeryio <summeryio@summeryiodeiMac.local>
2020-12-21 23:35:52 +0800

    按需引入element-ui / 支持scss

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import {Button} from 'element-ui'
+Vue.use(ElementUI)
+Vue.use(Button)
+
 Vue.config.productionTip = false;
 
 new Vue({
